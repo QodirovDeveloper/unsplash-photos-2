1eab4095632d07908ad938ae4df9e8499d2a53d5
QodirovDeveloper <startfly58@gmail.com>
2025-07-27 21:45:47 +0500

    add

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,42 @@
-// styles
+import { useState } from "react";
 import styles from "./Auth.module.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
   return (
     <div className={styles.formWrapper}>
-      <form className={styles.form}>
+      <form onSubmit={handleSubmit} className={styles.form}>
         <h1 className={styles.title}>Login</h1>
+
         <label>
           <span>Email:</span>
-          <input type="email" placeholder="email" />
-        </label>
-        <label>
-          <span>Password:</span>
-          <input type="password" placeholder="password" />
+          <input
+            type="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
 
-        <button>Login</button>
+        <label>
+          <span>Password:</span>
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+
+        <button type="submit">Login</button>
       </form>
     </div>
   );
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,26 +1,56 @@
-// styles
+import { useState } from "react";
 import styles from "./Auth.module.css";
 
 function Signup() {
-  
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Display Name:", displayName);
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+  };
+
   return (
     <div className={styles.formWrapper}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <h1 className={styles.title}>Signup</h1>
+
         <label>
           <span>Display Name:</span>
-          <input type="text" placeholder="display name" />
-        </label>
-        <label>
-          <span>Email:</span>
-          <input type="email" placeholder="email" />
-        </label>
-        <label>
-          <span>Password:</span>
-          <input type="password" placeholder="password" />
+          <input
+            type="text"
+            placeholder="display name"
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
+          />
         </label>
 
-         <button>Signup</button>
+        <label>
+          <span>Email:</span>
+          <input
+            type="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <span>Password:</span>
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+
+        <button type="submit">Signup</button>
       </form>
     </div>
   );
